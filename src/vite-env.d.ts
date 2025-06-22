0d7679ba3bf46d507bb79b7c8c0fcfdf0a428a87
Sungwook Kim <silenc3@naver.com>
2025-06-23 00:57:27 +0900

    chore: remove svgr setting & package

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,9 +4,3 @@ declare module '*.module.scss' {
   const classes: { [key: string]: string }
   export default classes
 }
-
-declare module '*.svg?react' {
-  import React from 'react'
-  const content: React.ComponentType<React.SVGProps<SVGSVGElement>>
-  export default content
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
